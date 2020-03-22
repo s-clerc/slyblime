7c71624aa088d8a21b8167ff45e82265e97db8a0
Sam <sam@swissnetizen.ch>
2020-03-23 00:42:41 +0100

    Make `reception` only returns the complete message

--- swank client.py
+++ swank client.py
@@ -35,6 +35,10 @@ class SwankClientProtocol(Dispatcher, asyncio.Protocol):
         "disconnect"
     ]
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.partial_message = None
+
     def connection_made(self, transport):
         self.transport = transport
         self.emit("connect")
@@ -42,16 +46,39 @@ class SwankClientProtocol(Dispatcher, asyncio.Protocol):
     def connection_lost(self, something):
         self.emit("disconnect", something)
 
+    def complete_data(self, data):
+        if self.partial_message is None:
+            packet_size = int(data[0:6].decode("utf-8"), 16)
+            if len(data)-6 < packet_size:
+                self.partial_message = data
+                return None
+            else:  # Data is already complete
+                return data
+        else:
+            self.partial_message += data
+            packet_size = int(self.partial_message[0:6].decode("utf-8"), 16)
+            if len(self.partial_message)-6 < packet_size:
+                return None
+            else:  # Data is finally complete
+                data = self.partial_message
+                self.partial_message = None
+                return data
+
     def data_received(self, data):
+        data = self.complete_data(data)
+        if (data is None): return
         packet_size = int(data[0:6].decode("utf-8"), 16)
-        print(packet_size)
         print(data)
         self.emit("reception", data[6:packet_size + 6])
+
         remainder = data[packet_size + 6:]
-        if len(remainder) > 5:
+        if len(remainder) > 5:  # sanity check
             print("Remainder: ")
             print(remainder)
             self.data_received(remainder)
+        elif len(remainder) > 0:
+            print("Erroneous remainder of ")
+            print(remainder)
 
     def write(self, message):
         output = message.encode("utf-8")
