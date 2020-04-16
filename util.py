09c0b8c92ef6b5dc770ff22308089dbec7a35c40
Sam <sam@swissnetizen.ch>
2020-04-17 01:44:41 +0200

    Add ability to determine package that a point is in

--- util.py
+++ util.py
@@ -1,4 +1,8 @@
 import asyncio
+from bisect import bisect_left
+import re
+
+from .sexpdata import loads, dumps
 
 def get_if_in(dictionary, *items):
     return tuple(dictionary[item] if item in dictionary else None 
@@ -35,3 +39,46 @@ async def show_quick_panel(session, items, flags, selected_index=0, on_highlight
     session.window.show_quick_panel(items, on_done, flags, selected_index, on_highlighted)
     await future
     return future.result()
+
+PACKAGE_REGEX = r"(?i)^\((cl:|common-lisp:)?in-package\ +[ \t']*([^\)]+)[ \t]*\)"
+IN_PACKAGE_REGEX = re.compile(r"(?i)(cl:|common-lisp:)?in-package\ +[ \t']*")
+
+# I misunderstood what SLY-CURRENT-PACKAGE did and wrote this
+# but use the one below this one as this one is not tested.
+def determine_package_at_point(view, slynk, point):
+    possibilities = view.find_all(PACKAGE_REGEX)
+    i = bisect_left(
+        [possibility[1] for possibility in possibilities], 
+        point) - 1
+    if i < 0:
+        return None
+    # Ignore the IN-PACKAGE symbol.
+    statement = loads(view.substr(possibilities[i]))[1:]
+    lisp = slynk.connexion_info.lisp_implementation.name
+    
+    ignore_next = False
+    for symbol in statement:
+        prefix = (symbol := str(symbol))[0:2]
+
+        if ignore_next: 
+            ignore_next = False
+        elif symbol == f"#-{lisp}":
+            ignore_next = True
+        elif prefix == "#+" and symbol != f"#+{lisp}":
+            ignore_next = True
+        else:
+            return symbol
+
+# Equivalent to SLY-CURRENT-PACKAGE in output.
+def in_package_parameters_at_point(view, point):
+    possibilities = view.find_all(PACKAGE_REGEX)
+    i = bisect_left(
+        [possibility.begin() for possibility in possibilities], 
+        point) - 1
+    if i < 0:
+        return None
+    # Remove the IN-PACKAGE symbol.
+    return IN_PACKAGE_REGEX.sub("", view.substr(possibilities[i])[1:-1])
+
+
+
