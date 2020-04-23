3356589db1d0f8b5fd1c2d02baf9ccbcaa8c8176
Sam <sam@swissnetizen.ch>
2020-04-23 18:29:27 +0200

    Improve PEP 8 compliance

--- repl.py
+++ repl.py
@@ -17,35 +17,36 @@ from . import pydispatch
 
 def prepare_backtrack_phantom(region, regions_index, value_index):
     [prefix, infix, postfix] = settings().get("repl")['backtracking']["affixes"]
-    return Phantom(region, 
-                    f"{prefix}{regions_index}{infix}{value_index}{postfix}", 
-                    LAYOUT_INLINE)
+    return Phantom(region,
+                   f"{prefix}{regions_index}{infix}{value_index}{postfix}",
+                   LAYOUT_INLINE)
 
 class ReplWrapper(repl.Repl):
     def __init__(self, slynk_repl):
         super().__init__("utf-8")
         self.slynk_repl = slynk_repl
         self._killed = False
-        
+
     # Sublime REPL specific
     def read_bytes(self):
         return None
-        
+
     def write(self, to_write):
         print(to_write)
         self.slynk_repl.process(to_write)
 
 
 class EventBasedReplView(sublimerepl.ReplView):
-    NEWLINE_SEQUENCE = "\n" # To allow for future configurability
+    NEWLINE_SEQUENCE = "\n"  # To allow for future configurability
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.repl.slynk_repl.bind(write_string=self.on_print,
-                                  write_values=self.on_write_values,
-                                  prompt=self.on_prompt,
-                                  evaluation_aborted=self.on_evaluation_aborted,
-                                  server_side_repl_close=self.on_server_side_repl_close)
+        self.repl.slynk_repl.bind(
+            write_string=self.on_print,
+            write_values=self.on_write_values,
+            prompt=self.on_prompt,
+            evaluation_aborted=self.on_evaluation_aborted,
+            server_side_repl_close=self.on_server_side_repl_close)
         self.repl.slynk_repl.play_events()
         self.value_phantom_groups = []
         self.backtrack_phantom_set = PhantomSet(self._view, "backtracking")
@@ -55,7 +56,7 @@ class EventBasedReplView(sublimerepl.ReplView):
         return True
 
     def get_final_character(self):
-        return self._view.substr(self._view.size()-1)
+        return self._view.substr(self._view.size() - 1)
 
     # Works exactly like (FRESH-LINE) in CL
     def fresh_line(self):
@@ -124,7 +125,7 @@ class EventBasedReplView(sublimerepl.ReplView):
         self.backtrack_phantom_set.update(phantoms)
 
     def hide_backtrack_phantoms(self):
-        self.backtrack_phantom_set.update([]) 
+        self.backtrack_phantom_set.update([])
 
 
 async def create_main_repl(session):
@@ -135,13 +136,16 @@ async def create_main_repl(session):
         repl = await slynk.create_repl()
         found = None
         for view in window.views():
-            break # I don't know what to do here so break
+            break  # I don't know what to do here so break
             if view.id() == "something":
                 found = view
                 break
         view = found or window.new_file()
         try:
-            rv = EventBasedReplView(view, ReplWrapper(repl), settings().get("repl")["syntax"], None)
+            rv = EventBasedReplView(
+                view, 
+                ReplWrapper(repl), 
+                settings().get("repl")["syntax"], None)
         except Exception as e:
             self.window.status_message(f"REPL-spawning failure {str(e)}")
         #rv.call_on_close.append(self._delete_repl)
@@ -165,18 +169,18 @@ class CreateReplCommand(sublime_plugin.WindowCommand):
 
 class ReplNewlineCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        if not (repl_view := sublimerepl.manager.repl_view(self.view)):
+        if not (repl_view := sublimerepl.manager.repl_view(self.view)): 
             return
         view = repl_view._view
         selection = view.sel()
-        caret_point = selection[len(selection)-1].begin()
+        caret_point = selection[len(selection) - 1].begin()
         view.insert(edit, caret_point, "\n")
 
 
 class SlyReplListener(sublime_plugin.EventListener):
     def on_modified(self, view):
         if not (view.settings().get("is_sly_repl")
-                and (repl_view := sublimerepl.manager.repl_view(view))):
+                and (repl_view := sublimerepl.manager.repl_view(view))): 
             return
         try:
             caret_point = view.sel()[0].begin()
