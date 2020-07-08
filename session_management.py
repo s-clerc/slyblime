d1de643e87e761090bd60c2e550d85e34bdd887b
Sam de Clerc <jaimelelait@icloud.com>
2020-07-08 16:50:09 +0200

    Add UI to control which window has which session

---
+++ session_management.py
@@ -0,0 +1,54 @@
+from sublime import *
+import sublime_plugin, asyncio
+from .sly import *
+from . import util
+
+
+def prepare_preview(session):
+    print("prep")
+    slynk = session.slynk
+    lisp = slynk.connexion_info.lisp_implementation
+
+    try: lisp_info = lisp.name + " " + lisp.version
+    except e: lisp_info = "Error determining name"
+
+    try: port_info = slynk.host + ":" + slynk.port + " on " + slynk.connexion_info.machine.instance
+    except e: port_info = "Error determining connexion information"
+
+    try: repl_info = str(len(session.repl_views)) + " REPLs opened"
+    except e: repl_info = "Unknown number of open REPLs"
+
+    return [lisp_info, port_info]
+
+
+async def session_choice(loop, window):
+  try:
+    try:
+        default_session = sessions.sessions.index(sessions.get_by_window(window, False, False))
+    except e:
+        default_session = 0
+    print("OEUA")
+    print([prepare_preview(session) for session in sessions.sessions])
+    choice = await util.show_quick_panel(
+        loop,
+        window,
+        [prepare_preview(session) for session in sessions.sessions],
+        0,
+        default_session)
+    return choice if choice != -1 else None
+  except e:
+    print(e)
+
+class SelectSessionCommand(sublime_plugin.WindowCommand):
+    def run(self, **kwargs):
+        asyncio.run_coroutine_threadsafe(
+            self.async_run(**kwargs),
+            loop)
+
+    async def async_run(self, **kwargs):
+        choice = await session_choice(loop, self.window)
+        if choice is None: return
+        sessions.set_by_window(window, sessions.sessions[session_choice])
+
+class CloseSessionCommand:
+    pass
